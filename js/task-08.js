91191ccea81ad9fd50a87f72a5d498dfdda30f0d
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-01-10 17:47:40 +0200

    Task_08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector('.login-form');
 
+
+
+const loginFormSubmit = (event) => {
+    event.preventDefault();
+  
+    const { elements: { email, password } } = event.currentTarget;
+    // console.log(email.value, password.value);
+
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    }
+    const fieldValues = {
+        Email: email.value, 
+        Password: password.value,
+};
+    console.log(fieldValues);
+    event.currentTarget.reset();
+}
+
+loginForm.addEventListener('submit', loginFormSubmit)
+
+
+// `Email: ${email.value}, Password: ${password.value}`
