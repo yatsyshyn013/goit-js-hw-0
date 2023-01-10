e6bf6e1d5cd8b49c5b5eeac396d0a7aca91c3d10
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-01-10 22:24:43 +0200

    Task_10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,53 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+
+const refs = {
+  boxes: document.querySelector('#boxes'),
+  input: document.querySelector('input'),
+  createBtn : document.querySelector('#controls button[data-create]'),
+  destroyBtn : document.querySelector('#controls button[data-destroy]'),
+}
+
+
+let amount;
+
+
+const onNumberBox = () => {
+    amount = Number(refs.input.value);
+    return amount;
+  }
+
+let size = 30;
+const box = [];
+
+
+const createBoxes = (amount) => {
+
+  for (let i = 0; i < amount; i++) {
+    refs.boxes.insertAdjacentHTML(
+      "beforeend",
+      `<div class="color" style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+    size = size + 10;
+    
+  }
+
+  refs.boxes.append(...box);
+  
+};
+
+
+
+
+const destroyBoxes = (amount) => {
+  refs.boxes.innerHTML = '';
+};
+
+refs.input.addEventListener('input', onNumberBox);
+refs.createBtn.addEventListener('click', () => createBoxes(amount));
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+
+
