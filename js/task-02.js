d1b36206371b0b9e99414d14866692072fc4ef4e
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-01-10 04:25:00 +0200

    Виконав Task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const addIngredientsOption = option => {
+  return option.map(option => {
+    const ingredientsEl = document.createElement('li');
+    ingredientsEl.textContent = option;
+    ingredientsEl.classList.add('item');
+
+    return ingredientsEl;
+    
+  })
+}
+
+const elements = addIngredientsOption(ingredients);
+// console.log(elements);
+
+const ingredientsContainer = document.querySelector('#ingredients');
+ingredientsContainer.append(...elements);
+
+// console.log(ingredientsContainer);
