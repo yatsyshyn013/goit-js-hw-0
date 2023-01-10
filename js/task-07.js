893d7d4d2acb4e4f535d2e81b82d0c53d93b4b1f
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-01-10 07:44:39 +0200

    Task_07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+
+const TextSizeControl = () => {
+text.style.fontSize = `${input.value}px`
+}
+
+input.addEventListener('input', TextSizeControl);
