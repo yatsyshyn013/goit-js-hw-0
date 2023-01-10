6a52a73727b3fc6bc803a76470725d26d71ede0d
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-01-10 18:42:18 +0200

    Task_09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const colorSpanEl = document.querySelector('.color');
+
+const changeBodyColor = (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorSpanEl.textContent = document.body.style.backgroundColor;
+  // colorSpanEl.innerHTML = `${document.body.style.backgroundColor}`;
+
+  // let hexColor = getRandomHexColor();
+  // document.body.style.backgroundColor = hexColor;
+  // colorSpanEl.textContent = hexColor;
+
+
+ 
+};
+
+buttonChangeColor.addEventListener('click', changeBodyColor);
